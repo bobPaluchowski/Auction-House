ea91ea5c5a437a2de31c9bedc40fb28c3d107def
bobPaluchowski <rpaluchowski@yahoo.com>
2023-01-22 12:56:55 +0000

    Styling Note and adding formatted Date

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,12 +1,14 @@
 import styles from "../styles/Note.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel, // This is the same as the Note model in the backend
+  className?: string, // Optional prop for styling
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
   const {
     title,
     text,
@@ -14,16 +16,26 @@ const Note = ({ note }: NoteProps) => {
     updatedAt
   } = note;
 
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  }
+
   return (
-    <Card className={styles.noteCard}>
-      <Card.Body>
-        <Card.Title className={styles.cardText}>
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
+        <Card.Title>
           {title}
         </Card.Title>
-        <Card.Text>
+        <Card.Text className={styles.cardText}>
           {text}
         </Card.Text>
       </Card.Body>
+      <Card.Footer className="text-muted">
+        {createdUpdatedText}
+      </Card.Footer>
     </Card>
   )
 }
